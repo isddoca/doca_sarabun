d34a7f17ba6da0a6db763fc9f41e4bcf8a3480da
Choatravee N <ch.n.17699@gmail.com>
2022-03-08 16:05:29 +0700

    ทดลองเรียก api ผ่านหน้าแรก

--- apps/doc_classify/apps.py
+++ apps/doc_classify/apps.py
@@ -7,8 +7,8 @@ from app import settings
 class DocClassifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'doc_classify'
-    D2V_FILE = os.path.join(settings.MODELS, "d2v-model_e250_v500_w5.pkl")
-    MODEL_FILE = os.path.join(settings.MODELS, "logreg-model_e250_v500_w5.pkl")
+    D2V_FILE = os.path.join(settings.MODELS, "d2v-model_e200_v500_w5.pkl")
+    MODEL_FILE = os.path.join(settings.MODELS, "logreg-model_e200_v500_w5.pkl")
     DICT_FILE = os.path.join(settings.RESOURCES, "dict.txt")
     d2v = pickle.load(open(D2V_FILE, 'rb'))
     model = pickle.load(open(MODEL_FILE, 'rb'))
--- apps/doc_record/views.py
+++ apps/doc_record/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -6,4 +7,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'doc_record/index.html')
+    data = {
+        'title':"รับผิดชอบการฝึกร่วม/ผสม คอบร้าโกลด์",
+        'unit':"กร.ทหาร",
+    }
+
+    URL = 'http://localhost:8000/classify'
+    result = requests.post(URL, data=data).json()
+
+    context ={
+        'title':data.get('title'),
+        'unit':data.get('unit'),
+        'receive_unit': ', '.join(result.get('received_units')),
+    }
+    return render(request, 'doc_record/index.html', context)
