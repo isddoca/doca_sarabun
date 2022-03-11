e1c3062f5005e2357c0e0f7c10ccd72e6fd247a2
Choatravee N <ch.n.17699@gmail.com>
2022-03-12 00:37:12 +0700

    create testcase document trace

--- doc_record/tests.py
+++ doc_record/tests.py
@@ -1,9 +1,9 @@
-from datetime import date
+from datetime import date, datetime
 
 from django.contrib.auth.models import User, Group
 from django.test import TestCase
 
-from .models import Doc
+from .models import Doc, DocTrace, DocStatus, Unit
 
 
 class docIdGenerator(TestCase):
@@ -17,12 +17,27 @@ class receiveDocTestCase(TestCase):
     def setUp(self):
         user = User.objects.create(id=1, username='0405', first_name="กรมกิจการพลเรือนทหารบก")
         group = Group.objects.create(id=1, name="กร.ทบ.")
+        group.unit.unit_id = "0405"
+        group.save()
         user.groups.add(group)
+        user.save()
+
+        budgetUser = User.objects.create(id=3, username='0405.3', first_name="กองโครงการ")
+        group3 = Group.objects.create(id=3, name="กคง.กร.ทบ.")
+        group3.unit.unit_id = "0405.3"
+        group3.save()
+        budgetUser.groups.add(group3)
+        budgetUser.save()
 
         no = 1
         doc_id = "{year}-{no}".format(year=date.today().year, no=f'{no:06}')
         Doc.objects.create(id=doc_id, doc_no="กห 0403/7588", doc_from="ยก.ทบ.", doc_to="กร.ทบ.",
-                                 title="เรื่องใหม่", create_by=user, active=True)
+                           title="เรื่องใหม่", create_by=user, active=True)
+        send = DocStatus.objects.create(id=1, name="ส่ง")
+        receive = DocStatus.objects.create(id=2, name="รับ")
+        DocTrace.objects.create(id=1, time=datetime.now(), action_by=user, doc_status=send,
+                                doc_id=doc_id)
+        DocTrace.objects.create(id=2, time=datetime.now(), action_by=budgetUser, doc_status=receive, doc_id=doc_id)
 
     def test_user(self):
         user2 = User.objects.get(id=1)
@@ -36,3 +51,8 @@ class receiveDocTestCase(TestCase):
         self.assertEqual(doc_test.doc_to, "กร.ทบ.")
         self.assertEqual(doc_test.title, "เรื่องใหม่")
         self.assertEqual(doc_test.create_by.id, user2.id)
+
+        doc_trace = DocTrace.objects.filter(doc_id=doc_test.id)
+        self.assertEqual(len(doc_trace), 2)
+        self.assertEqual(doc_trace[0].action_by.groups.all()[0].unit.unit_id, "0405")
+        self.assertEqual(doc_trace[1].action_by.groups.all()[0].unit.unit_id, "0405.3")
